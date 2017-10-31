8dfa77364c89e1266eb87a6cfcde214550f31145
LittleLeah <31472040LittleLeah@users.noreply.github.com>
2017-10-31 09:43:53 +0100

    mongodb

--- index.js
+++ index.js
@@ -2,15 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Mongoose = require("mongoose");
 var Express = require("express");
+var BlueBird = require("bluebird");
 var app = Express();
-Mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+Mongoose.Promise = BlueBird;
+Mongoose.connect('mongodb://127.0.0.1/log_db', { useMongoClient: true });
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    var KittySchema = new Mongoose.Schema({
-        name: String
+    var Log_Schema = new Mongoose.Schema({
+        action: String,
+        time: Date
     });
-    var Kitten = Mongoose.model("Kitten", KittySchema);
-    var silence = new Kitten({ name: "Silence" });
-    console.log(silence.name);
+    var date = new Date();
+    var Logs = Mongoose.model("Log", Log_Schema);
+    var log = new Logs({ action: "User logged in", time: date });
+    log.save(function (err) {
+        if (err)
+            return console.log(err);
+    });
+    console.log(log.action);
+    date = new Date();
+    log = new Logs({ action: "User added an transaction", time: date });
+    console.log(log.time);
 });
--- index.ts
+++ index.ts
@@ -1,19 +1,27 @@
 import * as Mongoose from "mongoose";
 import * as Express from "express";
+import * as BlueBird from "bluebird";
 
 let app = Express();
-
-Mongoose.connect('mongodb://localhost/test', {useMongoClient: true});
+(<any>Mongoose).Promise = BlueBird;
+Mongoose.connect('mongodb://127.0.0.1/log_db', {useMongoClient: true});
 
 let db = Mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-let KittySchema = new Mongoose.Schema({
-    name : String
+let Log_Schema = new Mongoose.Schema({
+    action : String,
+    time : Date
 });
-
-let Kitten = Mongoose.model("Kitten", KittySchema);
-let silence = new Kitten({ name: "Silence"});
-console.log(silence.name);
+let date = new Date();
+let Logs = Mongoose.model("Log", Log_Schema);
+let log = new Logs({ action: "User logged in", time: date});
+log.save(function (err) {
+if (err) return console.log(err);
+})
+console.log(log.action);
+date = new Date();
+log = new Logs({ action: "User added an transaction", time: date})
+console.log(log.time);
 });
