cc4358e2e8de23b74d3ce3ee66f099a84feb2d3a
LittleLeah <31472040LittleLeah@users.noreply.github.com>
2017-10-31 10:31:38 +0100

    bfiew

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var Express = require("express");
 var BlueBird = require("bluebird");
 var app = Express();
 Mongoose.Promise = BlueBird;
-Mongoose.connect('mongodb://127.0.0.1/log_db', { useMongoClient: true });
+Mongoose.connect('mongodb://LittleLeah:Fellini_2003@logdb-shard-00-00-pyaxd.mongodb.net:27017,logdb-shard-00-01-pyaxd.mongodb.net:27017,logdb-shard-00-02-pyaxd.mongodb.net:27017/test?ssl=true&replicaSet=LogDb-shard-0&authSource=admin', { useMongoClient: true });
 var db = Mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -23,5 +23,14 @@ db.once('open', function () {
     console.log(log.action);
     date = new Date();
     log = new Logs({ action: "User added an transaction", time: date });
+    log.save(function (err) {
+        if (err)
+            return console.log(err);
+    });
     console.log(log.time);
+    Logs.find({}, function (err, result) {
+        if (err)
+            console.log(err);
+        console.log(result);
+    });
 });
--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import * as BlueBird from "bluebird";
 
 let app = Express();
 (<any>Mongoose).Promise = BlueBird;
-Mongoose.connect('mongodb://127.0.0.1/log_db', {useMongoClient: true});
+Mongoose.connect('mongodb://LittleLeah:Fellini_2003@logdb-shard-00-00-pyaxd.mongodb.net:27017,logdb-shard-00-01-pyaxd.mongodb.net:27017,logdb-shard-00-02-pyaxd.mongodb.net:27017/test?ssl=true&replicaSet=LogDb-shard-0&authSource=admin', {useMongoClient: true});
 
 let db = Mongoose.connection;
 
@@ -23,5 +23,13 @@ if (err) return console.log(err);
 console.log(log.action);
 date = new Date();
 log = new Logs({ action: "User added an transaction", time: date})
+log.save(function (err) {
+    if (err) return console.log(err);
+    })
 console.log(log.time);
+
+Logs.find({}, function (err, result){
+    if (err) console.log(err);
+    console.log(result);
+});
 });
